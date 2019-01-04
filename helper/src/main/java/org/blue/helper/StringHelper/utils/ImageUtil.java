b4121cb5ff864dff2925ab8758aac48880332ef5
m15801983447@163.com <xrln47928>
2019-01-04 16:09:57 +0800

    自动识别图片

---
+++ helper/src/main/java/org/blue/helper/StringHelper/utils/ImageUtil.java
@@ -0,0 +1,16 @@
+package org.blue.helper.StringHelper.utils;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ * @Description <P></P>
+ * @Author v-Rui.Xiong@bl.com
+ * @Date 2019/1/4
+ * @Version 1.0.0
+ **/
+public class ImageUtil {
+    private static BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+    private static BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+
+}
---
+++ helper/src/main/java/org/blue/helper/StringHelper/utils/support/Base64.java
@@ -0,0 +1,12 @@
+package org.blue.helper.StringHelper.utils.support;
+
+/**
+ * @Description <P></P>
+ * @Author v-Rui.Xiong@bl.com
+ * @Date 2019/1/4
+ * @Version 1.0.0
+ **/
+public enum Base64 {
+    encoder,
+    decoder;
+}
